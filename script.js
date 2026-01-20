cc53f585951f913632812df21a24e03afbda26ae
hackersolo1 <143674895+hackersolo1@users.noreply.github.com>
2026-01-19 22:26:56 -0300

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -1,348 +1,127 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Header const's
-    const headerTitle = document.querySelector("#header-title");
-    const downloadBtn = document.querySelector('.downloadBtn');
-    const visuBtn = document.querySelector(".visu");
-    // --
+document.addEventListener('DOMContentLoaded', () => {
+    function previewWindow(active, atualText) {
+        document.querySelector('.preview-cont').style.animation = `${active} 0.3s ease-in-out forwards`;
+        setTimeout(() => {
+            visuBtn.innerText = `${atualText}`;
+        }, 100);
+    }
 
+    function switchTheme(themeKey) {
+        document.querySelector('.preview').style.background = THEMEINFO.theme[themeKey].background;
+        document.querySelector('.preview h2').style.color = THEMEINFO.theme[themeKey].h2;
+        document.querySelector('.preview h3').style.color = THEMEINFO.theme[themeKey].h3;
+        document.querySelector('.preview p').style.color = THEMEINFO.theme[themeKey].p;
+    }
 
-    // Input's const's
-    const parcInput = document.querySelector("#nameParc");
-    const mensageInput = document.querySelector("#mensageText");
-    const imageInput = document.querySelector("#imgUrl");
-    const musicInput = document.querySelector("#musicUrl");
-    // --
-
-
-    // Theme's const's
-    const whiteThemeBtn = document.querySelector(".white-theme");
-    const nightThemeBtn = document.querySelector(".night-theme");
-    const customThemeBtn = document.querySelector('.custom-theme');
-    // --
-
-
-    // Font's const's
-    const serifBtn = document.querySelector(".serif");
-    const sansBtn = document.querySelector(".sans");
-    const monoBtn = document.querySelector(".mono");
-    const cursivaBtn = document.querySelector(".cursiva");
-    // --
-
-
-    // Preview const's
-    const previewCont = document.querySelector(".preview-cont");
-    const preview = document.querySelector(".preview");
-    const previewImg = document.querySelector(".preview .img");
-    const previewH2 = document.querySelector(".preview h2");
-    const previewH3 = document.querySelector(".preview h3");
-    const previewP = document.querySelector(".preview p");
-    // --
-
-
-    // Custom theme const's
-    const customThemeTitleWindow = document.querySelector('.custom-title-window');
-    const customThemeSubtitleWindow = document.querySelector('.custom-subtitle-window');
-    const customThemeMensageWindow = document.querySelector('.custom-mensage-window');
-    const customThemeBackgroundWindow = document.querySelector('.custom-background-window');
-
-    const customTitle = document.querySelector('.custom-title-window h2');
-    const customSubtitle = document.querySelector('.custom-subtitle');
-    const customMensage = document.querySelector('.custom-mensage');
-    const customBackground = document.querySelector('.custom-background');
-    
-    const customTitleColorInput = document.querySelector('#custom-title-color-input');
-    const customSubtitleColorInput = document.querySelector('.custom-subtitle-color-input');
-    const customMensageColorInput = document.querySelector('.custom-mensage-color-input');
-    const customBackgroundColorInput = document.querySelector('.custom-background-color-input');
-
-    const customThemeBtnClose1 = document.querySelector('.custom-theme-btn-1-5');
-    const customThemeBtnClose2 = document.querySelector('.custom-theme-btn-2-5');
-    const customThemeBtnClose3 = document.querySelector('.custom-theme-btn-3-5');
-
-    const customThemeBtnNext = document.querySelector('.custom-theme-btn-1');
-    const customThemeBtnNext2 = document.querySelector('.custom-theme-btn-2');
-    const customThemeBtnNext3 = document.querySelector('.custom-theme-btn-3');
-    
-    const customThemeBtnEnd = document.querySelector('.custom-theme-btn-4');
-    // --
-
-
-    
-
-    // Default code
-    serifBtn.style.border = "4px solid white";
-    sansBtn.style.border = " 4px solid rgb(0, 35, 114)";
-    monoBtn.style.border = "4px solid rgb(0, 35, 114)";
-    cursivaBtn.style.border = "4px solid rgb(0, 35, 114)";
-
-    previewCont.style.fontFamily = "serif";
-
-    whiteThemeBtn.style.border = "4px solid black";
-
-    previewH2.style.color = "#4a4a4a";
-    previewH3.style.color = "#d63384";
-    previewP.style.color = "#2d2d2d";
-
-    downloadBtn.style.display = 'none';
-
-    preview.style.borderRadius = '20px';
-
-    customTitle.style.fontFamily = 'serif';
-    customSubtitle.style.fontFamily = 'serif';
-    customMensage.style.fontFamily = 'serif';
-    customBackground.style.fontFamily = 'serif';
-    // --
-
-
-    // Input's code's
-    parcInput.addEventListener("input", () => {
-        const parcInputValue = parcInput.value;
-        if (parcInputValue == "") {
-            previewH3.textContent = "O nome ficará assim";
-            customSubtitle.textContent = 'O nome ficará assim';
-        } else {
-            previewH3.textContent = parcInputValue;
-            customSubtitle.textContent = parcInputValue;
-        }
-    });
-
-    mensageInput.addEventListener("input", () => {
-        const mensageInputValue = mensageInput.value;
-        if (mensageInputValue == "") {
-            previewP.textContent =
-                "Esse texto é apenas uma alternativa para que você possa testar as fontes, imagens e tema. Assim que você preencher as informações necessárias (nome, mensagem e o link da imagem), esses textos serão substituidos...";
-        } else {
-            previewP.textContent = mensageInputValue;
-        }
-    });
-    // --
-
-
-    // Fonts Codes
-    serifBtn.addEventListener("click", () => {
-        serifBtn.style.border = "4px solid white";
-        sansBtn.style.border = " 4px solid rgb(0, 35, 114)";
-        monoBtn.style.border = "4px solid rgb(0, 35, 114)";
-        cursivaBtn.style.border = "4px solid rgb(0, 35, 114)";
-
-        previewCont.style.fontFamily = "serif";
-
-        customTitle.style.fontFamily = 'serif';
-        customSubtitle.style.fontFamily = 'serif';
-        customMensage.style.fontFamily = 'serif';
-        customBackground.style.fontFamily = 'serif';
-    });
-
-    sansBtn.addEventListener("click", () => {
-        sansBtn.style.border = "4px solid white";
-        serifBtn.style.border = "4px solid rgb(0, 35, 114)";
-        monoBtn.style.border = "4px solid rgb(0, 35, 114)";
-        cursivaBtn.style.border = "4px solid rgb(0, 35, 114)";
-
-        previewCont.style.fontFamily = "Lucida Sans";
-
-        customTitle.style.fontFamily = 'Lucida Sans';
-        customSubtitle.style.fontFamily = 'Lucida Sans';
-        customMensage.style.fontFamily = 'Lucida Sans';
-        customBackground.style.fontFamily = 'Lucida Sans';
-    });
-
-    monoBtn.addEventListener("click", () => {
-        monoBtn.style.border = "4px solid white";
-        serifBtn.style.border = "4px solid rgb(0, 35, 114)";
-        sansBtn.style.border = "4px solid rgb(0, 35, 114)";
-        cursivaBtn.style.border = "4px solid rgb(0, 35, 114)";
-
-        previewCont.style.fontFamily = "monospace";
-
-        customTitle.style.fontFamily = 'monospace';
-        customSubtitle.style.fontFamily = 'monospace';
-        customMensage.style.fontFamily = 'monospace';
-        customBackground.style.fontFamily = 'monospace';
-    });
-
-    cursivaBtn.addEventListener("click", () => {
-        cursivaBtn.style.border = "4px solid white";
-        serifBtn.style.border = "4px solid rgb(0, 35, 114)";
-        sansBtn.style.border = "4px solid rgb(0, 35, 114)";
-        monoBtn.style.border = "4px solid rgb(0, 35, 114)";
-
-        previewCont.style.fontFamily = "cursive";
-
-        customTitle.style.fontFamily = 'cursive';
-        customSubtitle.style.fontFamily = 'cursive';
-        customMensage.style.fontFamily = 'cursive';
-        customBackground.style.fontFamily = 'cursive';
-    });
-    // --
-
-
-    // Theme's codes
-    whiteThemeBtn.addEventListener("click", () => {
-        whiteThemeBtn.style.border = "4px solid black";
-        nightThemeBtn.style.border = "4px solid #0f172a";
-        customThemeBtn.style.border = '4px solid rgb(0, 35, 114)';;
-
-        previewH2.style.color = "#4a4a4a";
-        previewH3.style.color = "#d63384";
-        previewP.style.color = "#000000";
-        preview.style.background = "#ffebf0";
-    });
-
-    nightThemeBtn.addEventListener("click", () => {
-        nightThemeBtn.style.border = "4px solid white";
-        whiteThemeBtn.style.border = "4px solid #ffebf0";
-        customThemeBtn.style.border = '4px solid rgb(0, 35, 114)';
-
-        previewH2.style.color = "#ffd90070";
-        previewH3.style.color = "#ffd700";
-        previewP.style.color = "#e2e8f0";
-        preview.style.background = "#0f172a";
-    });
-
-
-    // Visu Anim
-    visuBtn.addEventListener("click", () => {
-        if (visuBtn.textContent == "Visualizar") {
-            previewCont.style.animation = "show 0.3s ease-in-out forwards";
-            downloadBtn.style.display = 'flex';
-            setTimeout(() => {
-                visuBtn.textContent = "Editor";
-            }, 100);
-        }
-
-        if (visuBtn.textContent == "Editor") {
-            previewCont.style.animation = "unshow 0.3s ease-in-out forwards";
-            downloadBtn.style.display = 'none';
-            setTimeout(() => {
-                visuBtn.textContent = "Visualizar";
-            }, 100);
-        }
-    });
-    // --
-
-
-    // Download Code 
-    downloadBtn.addEventListener('click', () => {
-        preview.style.borderRadius = '0px';
-        preview.style.height = 'auto';
-        html2canvas(preview).then(canvas => {
-            const img = canvas.toDataURL('imagem/png');
-            setTimeout(() => {
-                const link = document.createElement('a');
-                link.href = img;
-                link.download = 'carta-personalizada.png';
-                link.click();
-            }, 100);
+    function defaults() {
+        document.querySelector('.white-theme').style.border = '4px solid black';
+        switchTheme('white');
+        document.querySelectorAll('.sans, .mono, .cursiva').forEach(e => {
+            e.style.border = THEMEINFO.border.borderOff;
         });
+        document.querySelector('.serif').style.border = THEMEINFO.border.borderOn;
+    }
 
-        setTimeout(() => {
-            preview.style.borderRadius = '20px';
-            preview.style.height = '100%'
-        }, 200);
-    });
-    // --
+    function switchBorder(whiteBtn, nightBtn) {
+        document.querySelector('.white-theme').style.border = whiteBtn;
+        document.querySelector('.night-theme').style.border = nightBtn;
+    }
 
+    const THEMEINFO = {
+        theme: {
+            white: {
+                background: '#ffebf0',
+                h2: '#4a4a4a',
+                h3: '#d63384',
+                p: '#000000',
+            },
 
-    // Custom Theme Code's
+            night: {
+                background: '#0f172a',
+                h2: '#ffd90070',
+                h3: '#ffd700',
+                p: '#e2e8f0',
+            },
+        },
 
-    // - Custom theme Btn's code's
-    customThemeBtn.addEventListener('click', () => {
-        whiteThemeBtn.style.border = '4px solid white';
-        nightThemeBtn.style.border = '4px solid #0f172a';
-        customThemeBtn.style.border = '4px solid white';
+        fonts: {
+            serif: 'serif',
+            sans: 'Lucida Sans',
+            mono: 'monospace',
+            cursiva: 'cursive',
+        },
 
-        customThemeTitleWindow.style.animation = 'show2 0.3s ease-in-out forwards';
+        border: {
+            borderOn: '4px solid white',
+            borderOff: '4px solid rgb(0, 35, 114)',
+        }
+    }
 
-        const customTitleColorInputValue = customTitleColorInput.value;
-        const customSubtitleColorInputValue = customSubtitleColorInput.value;
-        const customMensageColorInputValue = customMensageColorInput.value;
-        const customBackgroundColorInputValue = customBackgroundColorInput.value;
+    const visuBtn = document.querySelector('.visu');
 
-        previewH2.style.color = customTitleColorInputValue;
-        previewH3.style.color = customSubtitleColorInputValue;
-        previewP.style.color = customMensageColorInputValue;
-        preview.style.background = customBackgroundColorInputValue;
-    });
-   
-    customThemeBtnNext.addEventListener('click', () => {
-        customThemeTitleWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
-        setTimeout(() => {
-            customThemeSubtitleWindow.style.animation = 'show2 0.3s ease-in-out forwards';
-        }, 400);
-    });
-
-    customThemeBtnNext2.addEventListener('click', () => {
-        customThemeSubtitleWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
-        setTimeout(() => {
-            customThemeMensageWindow.style.animation = 'show2 0.3s ease-in-out forwards';
-        }, 400);
-    });
-
-    customThemeBtnNext3.addEventListener('click', () => {
-        customThemeMensageWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
-        setTimeout(() => {
-            customThemeBackgroundWindow.style.animation = 'show2 0.3s ease-in-out forwards';
-        }, 400);
+    visuBtn.addEventListener('click', () => {
+        if (visuBtn.innerText == 'Visualizar') {
+            previewWindow('show', 'Editor');
+        }
+        if (visuBtn.innerText == 'Editor') {
+            previewWindow('unshow', 'Visualizar');
+        }
     });
 
-    customThemeBtnEnd.addEventListener('click', () => {
-        customThemeBackgroundWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
-    })
-    // ---
-
-    
-    // - Custom theme input's code's
-    customTitleColorInput.addEventListener('input', () => {
-        const customTitleColorInputValue = customTitleColorInput.value;
-
-        customTitle.style.color = customTitleColorInputValue;
-        previewH2.style.color = customTitleColorInputValue;
+    document.querySelector('#nameParc').addEventListener('input', () => {
+        const nameParcValue = document.querySelector('#nameParc').value;
+        if (nameParcValue == '') {
+            document.querySelector('.preview h3').innerText = 'O nome ficará assim';
+        } else {
+            document.querySelector('.preview h3').innerText = nameParcValue;
+        }
     });
 
-    customSubtitleColorInput.addEventListener('input', () => {
-        const customSubtitleColorInputValue = customSubtitleColorInput.value;
-
-        customSubtitle.style.color = customSubtitleColorInputValue;
-        previewH3.style.color = customSubtitleColorInputValue;
+    document.querySelector('#mensageText').addEventListener('input', () => {
+        const mensageTextValue = document.querySelector('#mensageText').value;
+        if (mensageTextValue == '') {
+            document.querySelector('.preview p').innerText = 'Esse texto é apenas uma alternativa para que você possa testar as fontes, imagens e tema. Assim que você preencher as informações necessárias (nome, mensagem e o link imagem) esses textos serão substituidos...';
+        } else {
+            document.querySelector('.preview p').innerText = mensageTextValue;
+        }
     });
 
-    customMensageColorInput.addEventListener('input', () => {
-        const customMensageColorInputValue = customMensageColorInput.value;
-
-        customMensage.style.color = customMensageColorInputValue;
-        previewP.style.color = customMensageColorInputValue;
+    document.querySelector('.white-theme').addEventListener('click', () => {
+        switchTheme('white');
+        switchBorder('4px solid black', '4px solid #0f172a', '4px solid rgb(0, 35, 114)');
     });
 
-    customBackgroundColorInput.addEventListener('input', () => {
-        const customBackgroundColorInputValue = customBackgroundColorInput.value;
-
-        customBackground.style.color = customBackgroundColorInputValue;
-        preview.style.background = customBackgroundColorInputValue;
-    })
-    // ---
-
-
-    // - Custom theme close btn's
-    customThemeBtnClose1.addEventListener('click', () => {
-        customThemeTitleWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
+    document.querySelector('.night-theme').addEventListener('click', () => {
+        switchTheme('night');
+        switchBorder('4px solid #ffebf0', '4px solid white', '4px solid rgb(0, 35, 114)')
     });
 
-    customThemeBtnClose2.addEventListener('click', () => {
-        customThemeSubtitleWindow.style.animation = 'unshow2 0.3s ease-in-out forwards';
+    document.querySelector('.font-cont').addEventListener('click', (e) => {
+        if (e.target.classList == 'serif') {
+            document.querySelectorAll('.sans, .mono, .cursiva').forEach(fontsItens => {
+                fontsItens.style.border = THEMEINFO.border.borderOff;
+            });
+            document.querySelector('.preview').style.fontFamily = THEMEINFO.fonts.serif;
+            document.querySelector('.serif').style.border = THEMEINFO.border.borderOn;
+        } else if (e.target.classList == 'sans') {
+            document.querySelectorAll('.serif, .mono, .cursiva').forEach(fontsItens => {
+                fontsItens.style.border = THEMEINFO.border.borderOff;
+            });
+            document.querySelector('.preview').style.fontFamily = THEMEINFO.fonts.sans;
+            document.querySelector('.sans').style.border = THEMEINFO.border.borderOn;
+        } else if (e.target.classList == 'mono') {
+            document.querySelectorAll('.sans, .serif, .cursiva').forEach(fontsItens => {
+                fontsItens.style.border = THEMEINFO.border.borderOff;
+            });
+            document.querySelector('.preview').style.fontFamily = THEMEINFO.fonts.mono;
+            document.querySelector('.mono').style.border = THEMEINFO.border.borderOn;
+        } else if (e.target.classList == 'cursiva') {
+            document.querySelectorAll('.sans, .mono, .serif').forEach(fontsItens => {
+                fontsItens.style.border = THEMEINFO.border.borderOff;
+            });
+            document.querySelector('.preview').style.fontFamily = THEMEINFO.fonts.cursiva;
+            document.querySelector('.cursiva').style.border = THEMEINFO.border.borderOn;
+        }
     });
-
-    customThemeBtnClose3.addEventListener('click', () => {
-        customThemeMensageWindow.style.animation = 'unshow 0.3s ease-in-out forwards';
-    });
-    // ---
-
-
-    // Img code's 
-    imageInput.addEventListener('change', () => {
-        const file = imageInput.files[0];
-        const link = URL.createObjectURL(file);      
-        previewImg.style.background = `url(${link})`;
-    });
-    // --
-});
+    defaults();
+})
